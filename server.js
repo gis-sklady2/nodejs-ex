e532f1f12529a5c13802e2387f40ed8b648c3623
vojtar <vojtar@gis-stavinvex.cz>
2016-11-16 14:41:02 +0100

    1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ Object.assign=require('object-assign')
 
 app.set("view engine", "ejs");
 app.use(morgan('combined'))
-
+app.use(express.static(__dirname + "/public"));
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
